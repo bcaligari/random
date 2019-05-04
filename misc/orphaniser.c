7dfaa4f85e4932145ce739f9bd478fb3946db6b5
Brendon Caligari <caligari@cypraea.co.uk>
2019-05-04 22:40:23 +0100

    ...

--- misc/orphaniser.c
+++ misc/orphaniser.c
@@ -12,7 +12,10 @@ int main(void)
 	char logmsg[128];
 
 	old_ppid = getpid();
-	printf("%d: is parent\n", (int) old_ppid);
+	printf("%d: is parent [pgid = %d; sid = %d]\n",
+		(int) old_ppid,
+		(int) getpgid((pid_t) 0),
+		(int) getsid((pid_t) 0));
 	for (i = 0; i < ORPHAN_COUNT; i++) {
 		pid = fork();
 		if (!pid) {
@@ -21,7 +24,11 @@ int main(void)
 				new_ppid = getppid();
 				if (new_ppid != old_ppid) {
 					openlog(NULL, LOG_PID, LOG_USER);
-					sprintf(logmsg, "ppid changed from %d to %d\n", old_ppid, new_ppid);
+					sprintf(logmsg, "ppid changed from %d to %d [pgid = %d; sid = %d]\n",
+						old_ppid,
+						new_ppid,
+						(int) getpgid((pid_t) 0),
+						(int) getsid((pid_t) 0));
 					syslog(LOG_DEBUG, logmsg);
 					closelog();
 					old_ppid = new_ppid;
@@ -31,7 +38,10 @@ int main(void)
 		} else if (pid == -1) {
 			perror("fork");
 		}
-		printf("%d: is a child\n", (int) pid);
+		printf("%d: is a child [pgid = %d; sid = %d]\n",
+			(int) pid,
+			(int) getpgid((pid_t) 0),
+			(int) getsid((pid_t) 0));	
 	}
 	printf("%d: will return after sleep(2) ...\n", old_ppid);
 	sleep(2);
